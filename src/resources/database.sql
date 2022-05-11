3d2e8ad3b0ddb108b1a33692c872f4e891aacb11
guyongtao <guyongtao@qq.com>
2022-05-11 20:52:41 +0800

    feature:优化
    Signed-off-by: guyongtao <guyongtao@qq.com>

--- src/resources/database.sql
+++ src/resources/database.sql
@@ -1,3 +1,3 @@
 -- 创建数据库
-CREATE DATABASE monitor_forecast DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE monitor_forecast_dev DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
